0d2740411c13835c57c21e5c1fad0124c24ba087
profchenmu <profchenmu@macdeMacBook-Pro.local>
2018-10-11 21:29:27 +0800

    fix

--- public/javascripts/users.js
+++ public/javascripts/users.js
@@ -4,7 +4,17 @@ window.onload = function() {
 		type: 'GET',
 		dataType: 'json',
 		success: function(data){
+			// const list = document.querySelector('.list')
+			// data.map((e, i) => {
+			// 	const item = document.createElement('div');
+			// 	item.innerText = `${e.name} - ${e.type}`;
+			// 	list.appendChild(item);
+			// })
 			console.log(data);
+			$.each(data, (i, e) => {
+				const $item = $('<div class="item"></div>').text(`${e.name} - ${e.type}`);
+				$('.list').append($item);
+			})
 		}
 	});
 }
