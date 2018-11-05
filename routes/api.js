a4520c1a534fad363a036964dfde122dab837cc2
profchenmu <profchenmu@macdeMacBook-Pro.local>
2018-11-05 19:34:44 +0800

    fix

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,39 @@
+basicUrl = 'http://106.14.195.192:3000/api'
+
+header: {
+    "Content-Type": "application/json",
+    "X-App-Name": "appName",
+    "Authorization": 'testToken'
+}
+{
+    url: '/profile/openid&code='+ '',
+    method: 'POST',
+    success: {
+        msg: 'successful',
+        code: 1,
+        data: {
+            openid: result.openid,
+            token: 'testToken'
+        }
+    }
+}
+
 {
     url: '/lottery/add',
     method: 'POST',
     data: {
-        rewardAccount: 1,
-        name: "test",
-        openType: 1,
-        countForType: 2,
-        lotteryOrder: 2,
-        sponcerDesc: "aaaaaa",
-        sponcerName: "aaaaa",
-        sponcerId: 1,
-        startTime: '2018-10-31 00:34:05',
-        endTime: '2018-11-30 00:34:12',
-        imgUrl: 'http://aa.bb.com/foo/bar.jpg',
-        isPublic: true,
+        rewardAccount: 1, //奖品数量
+        name: "test", //抽奖名称
+        openType: 1, //开奖方式，目前就3种，1：时间 | 2：数量 | 3：自定义
+        countForType: 2, //如果开奖方式为数量（2）时，字段必传
+        lotteryOrder: 2, //排序
+        sponcerDesc: "aaaaaa", //赞助商说明
+        sponcerName: "aaaaa", //赞助商名称
+        sponcerId: 1, //赞助商id
+        startTime: '2018-10-31 00:34:05', // 开始时间
+        endTime: '2018-11-30 00:34:12', // 结束时间
+        imgUrl: 'http://aa.bb.com/foo/bar.jpg', // 图片地址
+        isPublic: true, //是否公开
     },
     success: {
         msg: 'successful',
@@ -22,15 +42,15 @@
 }
 
 {
-    url: '/basicAccount/:userId',
+    url: '/profile/basicAccount/:userId',
     method: 'GET',
     success: {
         msg: 'successful',
         code: 1,
         data: {
-            lotteryJoinAccount: 2,
-            lotteryStartAccount: 2,
-            lotteryGetAccount: 2,
+            lotteryJoinAccount: 2, //参加的抽奖数
+            lotteryStartAccount: 2, //发起的抽奖数
+            lotteryGetAccount: 2, //中奖数
         } 
     }
 }
@@ -255,7 +275,7 @@
 
 
 {
-    url: '/address/detail',
+    url: '/profile/address/detail',
     method: 'GET',
     data: {
         userId: 'test1',
@@ -274,7 +294,7 @@
 }
 
 {
-    url: '/address/edit',
+    url: '/profile/address/edit',
     method: 'PUT',
     data: {
         userId: 'test1',
@@ -291,7 +311,7 @@
 }
 
 {
-    url: '/address/add',
+    url: '/profile/address/add',
     method: 'POST',
     data: {
         name: 'abc',
